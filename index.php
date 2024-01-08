17a70c04853d4b59bd62c289873f0506d35b6477
stanley <offorstanley4real@gmail.com>
2024-01-07 21:07:24 -0800

    changes made

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | Cadence Partner</title>
+    <title>Home | Cadence Partner Limited</title>
     <!-- Bootstrap -->
     <link href="main/assets/css/bootstrap.min.css" rel="stylesheet">
     
